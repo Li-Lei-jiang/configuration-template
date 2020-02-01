733b18fd3dfd126b4f545adcdbb3e192a43dcb6b
Li-Lei-jiang <948704474@qq.com>
2020-02-01 19:51:38 +0800

    项目基本配置模板

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    home
+    <top></top>
+    <button @click="open">按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {},
+  mounted() {
+    
+  },
+  methods: {
+    open() {
+      window.open('https://www.baidu.com?a=1')
+    }
+  },
+  watch: {},
+  computed: {}
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
